ce9e01ac319b35704cc7e39ea968c58e63fc2aba
kishorematrix <kishorematrix@gmail.com>
2024-03-19 15:50:11 +0530

    Changes made for Branch

--- src/main/java/javaBasics/ArrayList1.java
+++ src/main/java/javaBasics/ArrayList1.java
@@ -41,6 +41,12 @@ public class ArrayList1 {
 		System.out.println(cars.get(0));
 		System.out.println(cars.get(0));
 		System.out.println(cars.get(0));
+		////Testing GIT
+		
+		
+		System.out.println(cars.get(0));
+		System.out.println(cars.get(0));
+		System.out.println(cars.get(0));
 		cars.clear();
 		System.out.println(cars);
 
